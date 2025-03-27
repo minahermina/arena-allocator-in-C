d888dc4d1ec2d1193eb5c80a431e4ccb716357da
minaalbertsaeed <mina.albert33@gmail.com>
2025-03-28 00:10:10 +0200

    Edit main.c

--- main.c
+++ main.c
@@ -5,13 +5,21 @@
 #define ARENA_ALLOCATOR_IMPLEMENTATION
 #include "arena_allocator.h"
 
-#define ALLOC_SIZE 1000
+#define ALLOC_SIZE 10000
 int main(){
     Arena arena = {0};
-    arena_create(&arena, ALLOC_SIZE);
-    arena_dump(&arena);
-    char* arr = arena_alloc(&arena, 100);
+    arena_init(&arena, ALLOC_SIZE);
+
+
+    char* arr = (char*)arena_alloc(&arena, 100);
+    char* arr1 = (char*)arena_alloc(&arena, 1000);
+    char* arr2 = (char*)arena_alloc(&arena, 2000);
+    char* arr3 = (char*)arena_alloc(&arena, 3000);
+    char* arr4 = (char*)arena_alloc(&arena, 10000);
+
+    assert(arr4 != NULL);
 
     arena_destroy(&arena);
+
     return 0;
 }
