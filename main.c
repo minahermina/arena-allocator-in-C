315f47a1cfbba2c18b58fe8a4f5a1f83026b6e17
Mina Hermina <mina.albert.saeed@gmail.com>
2025-07-05 05:11:26 +0300

    Edit main.c

--- main.c
+++ main.c
@@ -2,37 +2,80 @@
 #include "arena_allocator.h"
 #include <stdio.h>
 
-int main() {
+#define NO_ELEMENTS 10
+
+typedef struct {
+    Arena *arena;
+    int **numbers;
+} Arg;
+
+void* alloc_numbers1(void * ptr)
+{
+    Arg *arg = (Arg*) ptr;
+    *(arg->numbers) = (int*) arena_alloc(arg->arena, NO_ELEMENTS * sizeof(int));
+
+    for (int i = 0; i < NO_ELEMENTS; i++) {
+        (*(arg->numbers))[i] = i;
+    }
+
+    printf("Numbers1: ");
+    for (int i = 0; i < NO_ELEMENTS; i++) {
+        printf("%d ", (*(arg->numbers))[i]);
+    }
+    printf("\n");
+    return  NULL;
+}
+
+void* alloc_numbers2(void * ptr)
+{
+    Arg *arg = (Arg*) ptr;
+    *(arg->numbers) = (int*) arena_alloc(arg->arena, NO_ELEMENTS * sizeof(int));
+
+    for (int i = 0; i < NO_ELEMENTS; i++) {
+        (*(arg->numbers))[i] = i * 2;
+    }
+
+    printf("Numbers1: ");
+    for (int i = 0; i < NO_ELEMENTS; i++) {
+        printf("%d ", (*(arg->numbers))[i]);
+    }
+    printf("\n");
+    return  NULL;
+}
+
+int main() 
+{
     // Initialize the arena
-    Arena my_arena = {0};
-    arena_init(&my_arena, ARENA_REGION_DEFAULT_CAPACITY);
+    Arena arena = {0};
+    pthread_t thread1, thread2;
+    int ret1, ret2;
+    arena_init(&arena, ARENA_REGION_DEFAULT_CAPACITY);
+    int *numbers1 = 0, *numbers2 = 0;
 
-    // Allocate memory using the arena
-    int *numbers = (int*)arena_alloc(&my_arena, 10 * sizeof(int));
-    char *string = (char*)arena_alloc(&my_arena, 100 * sizeof(char));
+    Arg arg1 = {&arena, &numbers1};
+    Arg arg2 = {&arena, &numbers2};
+    printf("numbers1 before: %p\n", numbers1);
 
-    if (!numbers || !string) {
-        printf("Memory allocation failed!\n");
-        return 1;
-    }
+    ret1 = pthread_create(&thread1, NULL,  alloc_numbers1, (void *) &arg1);
+    ret2 = pthread_create(&thread2, NULL,  alloc_numbers2, (void *) &arg2);
 
-    // Use the allocated memory
-    for (int i = 0; i < 10; i++) {
-        numbers[i] = i * i;
-    }
-    snprintf(string, 100, "Hello from the arena allocator!");
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
 
-    // Print allocated data
-    printf("Numbers: ");
-    for (int i = 0; i < 10; i++) {
-        printf("%d ", numbers[i]);
-    }
-    printf("\nString: %s\n", string);
+    printf("Thread 1 returns: %d\n", ret1);
+    printf("numbers1 after thread1: %p\n\n", numbers1);
+
+    printf("numbers1 start at: %p\n", numbers1);
+    printf("numbers1 ends at: %p\n\n", numbers1 + NO_ELEMENTS - 1);
+    printf("numbers2 start at: %p\n", numbers2);
+    printf("numbers2 ends at: %p\n\n", numbers2 + NO_ELEMENTS - 1);
+
+    printf("Thread 2 returns: %d\n", ret2);
 
     // Dump arena state (optional)
-    arena_dump(&my_arena);
+    arena_dump(&arena);
 
     // Deallocate the arena all at once
-    arena_destroy(&my_arena);
+    arena_reset(&arena);
     return 0;
 }
